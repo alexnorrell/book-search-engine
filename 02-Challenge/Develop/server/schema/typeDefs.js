d90737254d16f57e4e6ddfbec33742f9fd9237f8
alexnorrell <norrell.alex@gmail.com>
2023-03-09 18:19:14 -0500

    adding packages and working on schema

---
+++ 02-Challenge/Develop/server/schema/typeDefs.js
@@ -0,0 +1,42 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        savedBooks: [Book]
+        bookCount: Int
+    }
+    type Book {
+        _id: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: saveBookInput): User
+        removeBook(bookId: String!): User
+    }
+    type Auth{
+        token: ID!,
+        user: User
+    }
+    input saveBookInput {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+`
+
+module.exports = typeDefs
--- 02-Challenge/Develop/server/server.js
+++ 02-Challenge/Develop/server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schema')
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +26,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startServer = async (typeDefs, resolvers) => {
+  await server.start()
+  server.applyMiddleware({ app })
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`graphql running at localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+}
+
+
+startServer(typeDefs, resolvers)
